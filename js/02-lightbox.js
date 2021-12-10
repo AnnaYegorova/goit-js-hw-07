213e842c1d9c2879a2e838e7f3da508de105ea78
AnnaYegorova <good4anna@gmail.com>
2021-12-10 21:53:00 +0200

    fix task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,22 +1,17 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// console.log(galleryItems);
 const galleryContainer = document.querySelector(".gallery");
-
-// const galleryItemMarkup = createImageGalleryMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML(
   "beforeend",
   createImageGalleryMarkup(galleryItems)
 );
 
-galleryContainer.addEventListener("click", onImageClick);
-
 function createImageGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<div class="gallery__item">
+      return `<li class="gallery__item">
   <a class="gallery__link" href="${original}"">
   <img
     
@@ -26,24 +21,17 @@ function createImageGalleryMarkup(galleryItems) {
     alt="${description}""
   />
 </a>
-</div> `;
+</li> `;
     })
     .join("");
 }
 
-function onImageClick(event) {
-  event.preventDefault();
-  const isGalleryItem = event.target.classList.contains("gallery__image");
-  if (!isGalleryItem) {
-    return;
-  }
-  const lightbox = new SimpleLightbox(".gallery a", {
-    captions: true,
-    captionDelay: 0,
-    captionSelector: "img",
-    captionType: "attr",
-    captionsData: "title",
-    captionPosition: "bottom",
-    captionClass: "",
-  });
-}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 0,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "title",
+  captionPosition: "bottom",
+  captionClass: "",
+});
